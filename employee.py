adfc8eb794b17bda5be9334be189dc2fa030343f
Allie-source <142596932+Allie-source@users.noreply.github.com>
2023-11-16 16:49:45 -0500

    ex1 final
    gonna add comments to it

---
+++ employee.py
@@ -0,0 +1,43 @@
+class Employee:
+
+    def __init__(self, name, number):
+        self.__name = name
+        self.__number = number 
+
+    def set_name(self, name):
+        self.__name = name 
+
+    def set_number(self, number):
+        self.__number = number
+
+    def get_name(self):
+        return self.__name
+    
+    def get_number(self):
+        return self.__number
+    
+class ProductionWorker(Employee):
+
+    def __init__(self, name, number, shift, pay):
+
+        Employee.__init__(self, name, number)
+
+        self.__shift = shift
+        self.__pay = pay 
+    
+    def set_shift(self, shift):
+        self.__shift = shift 
+
+    def set_pay(self, pay):
+        self.__pay = pay
+
+    def get_shift(self):
+        if self.__shift == 1:
+            return 'Day Shift'
+        elif self.__shift == 2:
+            return 'Night Shift'
+        else:
+            return 'Shift Not Found'
+    
+    def get_pay(self):
+        return self.__pay 
